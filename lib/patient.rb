ead817e94c7b2c7134217360e3f9766383c33847
angelo <aurtula1@gmail.com>
2021-03-09 13:53:04 -0500

    commit tree 89eb6c934bdce11bf4ac7346619d2dc8b11893d8

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,26 @@
+class Patient
+    attr_accessor :name, :date, :doctor
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def new_appointment(date, doctor)
+        appointment = Appointment.new(date, self, doctor)
+    end
+
+    def self.all
+        @@all
+    end
+
+    def appointments
+        Appointment.all.select{|appointment| appointment.patient == self}
+    end
+
+    def doctors
+        appointments.map{|appointment| appointment.doctor}
+    end
+end
